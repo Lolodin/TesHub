38a18eb0285b1c8bfebaf54bdb995fc35a937401
Nik <ilii4@yandex.ru>
2018-12-05 11:15:23 +0300

    Update TestFormsHandler

--- public/JS/Tests.js
+++ public/JS/Tests.js
@@ -21,7 +21,8 @@ document.onclick = function (event) {
         newQuestion.id = "question" +objectQuest.indexQuest;
         newQuestion.innerHTML = '<input name="question' + objectQuest.indexQuest + '" value="Вопрос">' +
             '<br>' +
-            '<input name="' + objectQuest.indexQuest + 'answer' + (objectQuest.indexAnswer[objectQuest.indexQuest]) + '" value="Ответ">' +
+            '<input name="' + objectQuest.indexQuest + 'answer0[]' + '" value="Ответ">' +
+            '<input type="checkbox" name="' + objectQuest.indexQuest +'answer0[]" value="true">'+
             '<br>' +
             '<button id="'+objectQuest.indexQuest+'" class = "addAnswer">Добавить вариант ответа</button>' +
             '<br>' +
@@ -33,14 +34,20 @@ document.onclick = function (event) {
         event.preventDefault();
        var elem = target.id;
       var newAnswer = document.createElement('input');
- newAnswer.name = elem +"answer"+(objectQuest.indexAnswer[elem]+1);
-       objectQuest.indexAnswer[elem]++;
-newAnswer.value = 'ответ';
+      newAnswer.name =elem+"answer"+(objectQuest.indexAnswer[elem]+1)+'[]';
+      objectQuest.indexAnswer[elem]++;
+      newAnswer.value = 'ответ';
+      var checkbox = document.createElement('input');
+      checkbox.type = 'checkbox';
+      checkbox.name = newAnswer.name;
+      checkbox.value = 'true';
+
 
 var questSelector = document.getElementById('question'+elem);
 var brBefore = document.getElementById(elem);
 var br = document.createElement('br');
 questSelector.insertBefore(newAnswer, brBefore);
+questSelector.insertBefore(checkbox, brBefore);
 questSelector.insertBefore(br, brBefore)
 
 
